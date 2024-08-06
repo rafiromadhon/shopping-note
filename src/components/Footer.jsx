e49e5bcf6a4725aee41140a127fa19a4f1f7822f
= <rafiromadhon006@gmail.com>
2024-08-06 20:31:57 +0700

    Partitioning components

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer({items}){
+    const totalItems = items.length;
+    const checkedItems = items.filter((item) => item.checked).length;
+    const percentage = (totalItems === 0 && checkedItems === 0) ? 0 : Math.round((checkedItems / totalItems) * 100);
+
+    return <footer className="stats">Ada {totalItems} barang di daftar belanjaan, {checkedItems} barang sudah dibeli ({percentage}%)</footer>;
+}
